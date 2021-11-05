677a984edfb2a6ab5a3ffe557e3328aa494489df
Zulfaabam <63113235+Zulfaabam@users.noreply.github.com>
2021-11-05 22:07:15 +0700

    styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import "../styles/globals.css";
+import '../styles/globals.css'
+import '../styles/login.css'
+import '../styles/register.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
