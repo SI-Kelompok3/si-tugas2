a6f4d77392adab85355be8e606b601e2c88ccc3d
Syahrizal Fauzi <44159978+syahrizalfauzi@users.noreply.github.com>
2021-11-06 10:17:47 +0700

    Update sesi kehadiran done

---
+++ pages/api/sesi.js
@@ -0,0 +1,25 @@
+import { transaction } from "../../config/db";
+
+export default async (req, res) => {
+  switch (req.method) {
+    case "PUT":
+      const { data } = req.body;
+      let putResult = transaction();
+      data.forEach(
+        (d) =>
+          (putResult = putResult.query(
+            `UPDATE sesi SET 
+            hadir = ${d.hadir ? `1` : `0`},
+            ${
+              d.keterangan !== "" || d.keterangan !== null
+                ? `keterangan = '${d.keterangan}'`
+                : "keterangan = NULL"
+            }
+            WHERE mengikuti_id = ${d.mengikuti_id}`
+          ))
+      );
+      await putResult.commit();
+      res.json({ message: "Perubahan data sesi berhasil" });
+      break;
+  }
+};
--- pages/kelas/[kelas_id]/sesi/[materi]/[tanggal].jsx
+++ pages/kelas/[kelas_id]/sesi/[materi]/[tanggal].jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import Layout from '../../../../../components/Layout';
-import { getSesiDetail } from '../../../../../lib/queries';
-import withAuth from '../../../../../lib/withAuth';
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import Layout from "../../../../../components/Layout";
+import fetchJson from "../../../../../lib/fetchJson";
+import { getSesiDetail } from "../../../../../lib/queries";
+import withAuth from "../../../../../lib/withAuth";
 
 export async function getServerSideProps(context) {
   return withAuth(
@@ -9,36 +11,117 @@ export async function getServerSideProps(context) {
     async () => {
       const { materi, tanggal } = context.params;
       const data = await getSesiDetail(materi, tanggal);
-      return { props: { data } };
+      return {
+        props: {
+          data: data.map((d) => ({
+            ...d,
+            hadir: d.hadir === 0 ? false : true,
+          })),
+        },
+      };
     },
-    ['guru'],
+    ["guru"]
   );
 }
 
-const DetailSesi = ({ data }) => (
-  <Layout>
-    <h1>Daftar Kehadiran Peserta</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>No.</th>
-          <th>Nama</th>
-          <th>Hadir</th>
-          <th>Keterangan</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((peserta, index) => (
-          <tr key={index}>
-            <td>{index + 1}</td>
-            <td>{peserta.nama}</td>
-            <td>{peserta.hadir === '0' ? 'Tidak Hadir' : 'Hadir'}</td>
-            <td>{peserta.keterangan ?? '-'}</td>
+const DetailSesi = ({ data: initialData }) => {
+  const router = useRouter();
+  const [data, setData] = useState(initialData);
+
+  const handleChangeKehadiran = (hadir, mengikuti_id) =>
+    setData((state) => {
+      let newState = state;
+      const index = newState.findIndex((p) => p.mengikuti_id === mengikuti_id);
+      newState[index] = { ...newState[index], hadir };
+      return [...newState];
+    });
+
+  const handleChangeKeterangan = (e) =>
+    setData((state) => {
+      let newState = state;
+      const index = newState.findIndex(
+        (p) => p.mengikuti_id === Number(e.target.id)
+      );
+      newState[index] = { ...newState[index], keterangan: e.target.value };
+      return [...newState];
+    });
+
+  const handleKehadiranSemua = (hadir) =>
+    setData((state) => {
+      let newState = state.map((s) => ({ ...s, hadir }));
+      return [...newState];
+    });
+
+  const handleSave = async () => {
+    await fetchJson("/api/sesi", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ data }),
+    });
+    router.reload();
+  };
+
+  return (
+    <Layout>
+      <h1>Daftar Kehadiran Peserta</h1>
+      {data !== initialData && (
+        <>
+          <button onClick={handleSave}>Simpan</button>
+          <br />
+        </>
+      )}
+      <button onClick={() => handleKehadiranSemua(true)}>Hadir semua</button>
+      <button onClick={() => handleKehadiranSemua(false)}>
+        Tidak hadir semua
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>Kehadiran</th>
+            <th>Keterangan</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </Layout>
-);
+        </thead>
+        <tbody>
+          {data.map((peserta, index) => (
+            <tr key={peserta.mengikuti_id}>
+              <td>{index + 1}</td>
+              <td>{peserta.nama}</td>
+              <td>
+                <button
+                  disabled={!peserta.hadir}
+                  onClick={() =>
+                    handleChangeKehadiran(false, peserta.mengikuti_id)
+                  }
+                >
+                  X
+                </button>
+                <button
+                  disabled={peserta.hadir}
+                  onClick={() =>
+                    handleChangeKehadiran(true, peserta.mengikuti_id)
+                  }
+                >
+                  V
+                </button>
+                {peserta.hadir ? "Hadir" : "Tidak Hadir"}
+              </td>
+              <td>
+                <input
+                  type="text"
+                  id={peserta.mengikuti_id}
+                  onChange={handleChangeKeterangan}
+                  placeholder="Misal : Sakit, Ijin"
+                  value={peserta.keterangan ?? ""}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
 
 export default DetailSesi;
