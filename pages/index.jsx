4b8e92e5a4bd45d90282590e97f57636c47616dd
Zulfaabam <63113235+Zulfaabam@users.noreply.github.com>
2021-11-06 10:01:26 +0700

    styling home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,29 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
-import { getKelasForPeserta, getTopGuru, getTopPeserta } from "../lib/queries";
-import { capitalizeFirstLetter } from "../lib/utility";
-import withAuth from "../lib/withAuth";
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import { getKelasForPeserta, getTopGuru, getTopPeserta } from '../lib/queries'
+import { capitalizeFirstLetter } from '../lib/utility'
+import withAuth from '../lib/withAuth'
 
 export async function getServerSideProps(context) {
   return withAuth(context, async (user) => {
-    let data = null;
+    let data = null
     switch (user.role) {
-      case "admin":
-        const peserta = await getTopPeserta();
-        const guru = await getTopGuru();
+      case 'admin':
+        const peserta = await getTopPeserta()
+        const guru = await getTopGuru()
         data = {
           peserta,
           guru,
-        };
-        break;
-      case "peserta":
-        const kelas = await getKelasForPeserta(user.id);
+        }
+        break
+      case 'peserta':
+        const kelas = await getKelasForPeserta(user.id)
         data = {
           data: kelas,
-        };
-        break;
-      case "guru":
-        break;
+        }
+        break
+      case 'guru':
+        break
     }
 
     return {
@@ -31,106 +31,108 @@ export async function getServerSideProps(context) {
         data,
         user,
       },
-    };
-  });
+    }
+  })
 }
 
 const Home = ({ data, user }) => (
   <Layout>
-    <h1>Selamat datang {user.nama}!</h1>
-    {user.role === "peserta" && (
-      <div>
-        <b>Kelas yang sudah berjalan</b>
-        <table>
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>Nama</th>
-              <th>Hari</th>
-              <th>Waktu Mulai</th>
-              <th>Waktu Akhir</th>
-              <th>Jumlah Kehadiran</th>
-              <th>Nilai</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.data.map((kelas, index) => (
-              <tr key={kelas.id}>
-                <td>{index + 1}</td>
-                <td>
-                  <Link href={`/kelas/${kelas.id}`}>{kelas.nama}</Link>
-                </td>
-                <td>{capitalizeFirstLetter(kelas.hari)}</td>
-                <td>{kelas.waktu_mulai}</td>
-                <td>{kelas.waktu_akhir}</td>
-                <td>{kelas.jumlah_kehadiran}</td>
-                <td>{kelas.nilai ?? "-"}</td>
+    <div className="homepage">
+      <h1 className="welcome">Selamat datang, {user.nama}!</h1>
+      {user.role === 'peserta' && (
+        <div className="table-wrapper">
+          <b>Kelas yang sudah berjalan</b>
+          <table>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Nama</th>
+                <th>Hari</th>
+                <th>Waktu Mulai</th>
+                <th>Waktu Akhir</th>
+                <th>Jumlah Kehadiran</th>
+                <th>Nilai</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )}
-    {user.role === "admin" && (
-      <div>
-        <b>Kelas yang paling diminati</b>
-        <table>
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>Nama Kelas</th>
-              <th>Jumlah Peserta</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.peserta.map((kelas, index) => (
-              <tr key={kelas.id}>
-                <td>{index + 1}</td>
-                <td>
-                  <Link href={`/kelas/${kelas.id}`}>{kelas.nama}</Link>
-                </td>
-                <td>{kelas.jumlah_peserta}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <b>Kelas dengan jumlah guru terendah</b>
-        <table>
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>Nama Kelas</th>
-              <th>Jumlah Guru</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.guru.map((kelas, index) => (
-              <tr key={kelas.id}>
-                <td>{index + 1}</td>
-                <td>
-                  <Link href={`/kelas/${kelas.id}`}>{kelas.nama}</Link>
-                </td>
-                <td>{kelas.jumlah_guru}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )}
-    <ul>
-      <li>
-        <Link href="/peserta">Daftar Peserta</Link>
-      </li>
-      <li>
-        <Link href="/kelas">Daftar Kelas</Link>
-      </li>
-      {user.role === "admin" && (
-        <li>
-          <Link href="/guru">Daftar Guru</Link>
-        </li>
+            </thead>
+            <tbody>
+              {data.data.map((kelas, index) => (
+                <tr key={kelas.id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <Link href={`/kelas/${kelas.id}`}>{kelas.nama}</Link>
+                  </td>
+                  <td>{capitalizeFirstLetter(kelas.hari)}</td>
+                  <td>{kelas.waktu_mulai}</td>
+                  <td>{kelas.waktu_akhir}</td>
+                  <td>{kelas.jumlah_kehadiran}</td>
+                  <td>{kelas.nilai ?? '-'}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
-    </ul>
+      {user.role === 'admin' && (
+        <div className="table-wrapper">
+          <b>Kelas yang paling diminati</b>
+          <table>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Nama Kelas</th>
+                <th>Jumlah Peserta</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.peserta.map((kelas, index) => (
+                <tr key={kelas.id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <Link href={`/kelas/${kelas.id}`}>{kelas.nama}</Link>
+                  </td>
+                  <td>{kelas.jumlah_peserta}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <b>Kelas dengan jumlah guru terendah</b>
+          <table>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Nama Kelas</th>
+                <th>Jumlah Guru</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.guru.map((kelas, index) => (
+                <tr key={kelas.id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <Link href={`/kelas/${kelas.id}`}>{kelas.nama}</Link>
+                  </td>
+                  <td>{kelas.jumlah_guru}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      <ul>
+        <li>
+          <Link href="/peserta">Daftar Peserta</Link>
+        </li>
+        <li>
+          <Link href="/kelas">Daftar Kelas</Link>
+        </li>
+        {user.role === 'admin' && (
+          <li>
+            <Link href="/guru">Daftar Guru</Link>
+          </li>
+        )}
+      </ul>
+    </div>
   </Layout>
-);
+)
 
-export default Home;
+export default Home
